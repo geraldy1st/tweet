578841c872a406134ff8851106029099541e785b
geraldy1st <geraldy.leondas@gmail.com>
2023-07-14 09:07:46 -0400

    format true

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/, message: "invalid format"}
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "invalid format"}
 end
   
